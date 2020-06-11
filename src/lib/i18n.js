b8611cc43f0eaf5edbbce3940bf01292dbb88bae
VuVanHoc <hocvanvu1999@gmail.com>
2020-06-11 21:48:55 +0700

    add lib redux & redux thunk

--- src/lib/i18n.js
+++ src/lib/i18n.js
@@ -2,10 +2,11 @@ import * as Localization from "expo-localization";
 import i18n from "i18n-js";
 // Set the key-value pairs for the different languages you want to support.
 i18n.locale = Localization.locale;
+i18n.fallbacks = true;
+
 i18n.translations = {
   en: { cancel: "Cancle" },
   vi: { cancel: "Huỷ" },
 };
-i18n.fallbacks = true;
 
 export default i18n;
