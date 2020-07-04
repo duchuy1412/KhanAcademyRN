cb9bb4497e18a8b32ba41f39c1ee84df448f2672
HuyND <ngohuy99bn@gmail.com>
2020-07-04 08:35:37 +0700

    Add Language Screen

---
+++ src/screens/LanguageScreen.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { View, StyleSheet } from "react-native";
+import { Colors, Button, Text, Caption, List } from "react-native-paper";
+import _l from "../lib/i18n";
+import * as UserActions from "../actions/userActions";
+
+export default class LanguageScreen extends React.Component {
+  render() {
+    const { navigation } = this.props;
+
+    return (
+      <View style={{ flex: 1 }} theme={{ roundness: 3 }}>
+        <Text>okkkkkkkkkkkk</Text>
+      </View>
+    );
+  }
+}
